89a1b6374f0733a948a5fb7395d2a605e73b8732
parsaltem <altemuraltan@hotmail.com>
2024-04-14 12:59:55 +0300

    upload files

---
+++ README.md
@@ -0,0 +1,55 @@
+# Frontend Mentor - FAQ accordion solution
+
+This is a solution to the [FAQ accordion challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-wyfFdeBwBz). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Thoughts](#thoughts)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Hide/Show the answer to a question when the question is clicked
+- Navigate the questions and hide/show answers using keyboard navigation alone
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./ScreenShots/desktop.png)
+![](./ScreenShots/mobile.png)
+
+### Links
+
+[Solution URL](https:../)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Javascript
+- Mobile-first workflow
+
+### Thoughts
+
+This was the my first project using JS. I tried to keep it simple to read. First I created JS head and text objects within an data array. Then create HTML document. I did really enjoy while creating all documents. I hope you do too.
+
+I may update this solution in the future, until then, this element is an interesting thing to note, at least.
+
+## Author
+
+- Frontend Mentor - [@Parsaltem](https://www.frontendmentor.io/profile/Parsaltem)
---
+++ script.js
@@ -0,0 +1,62 @@
+const paragraphContainer = document.getElementById('paragraph-container');
+
+const textData = [
+    {
+    id: 1,
+    head: "What is Frontend Mentor, and how will it help me?",
+    text: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    },
+    {
+    id: 2,
+    head: "Is Frontend Mentor free?",
+    text: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+    },
+    {
+    id: 3,
+    head: "Can I use Frontend Mentor projects in my portfolio?",
+    text: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+    },
+    {
+    id: 4,
+    head: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    text: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
+    },
+]
+
+const fulfillWithText = () => {
+
+    const textContainer = textData.map(({id, head, text}) => {
+        return `
+        <div class="heads" id="${id}-paragraph">
+            <h2>${head}</h2>
+            <img class="plus_icon" src="assets/images/icon-plus.svg" alt="plus-icon">
+            <img class="minus_icon hide" src="assets/images/icon-minus.svg" alt="minus-icon">
+        </div>
+        <p class="hide">${text}</p>
+        <hr>
+        `;
+    }).join('');
+
+    paragraphContainer.innerHTML = textContainer;
+
+    const plusIcons = document.querySelectorAll('.plus_icon');
+    Array.from(plusIcons).forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.target.classList.toggle('hide');
+            e.target.parentElement.nextElementSibling.classList.toggle('hide');
+            e.target.parentElement.querySelector('.minus_icon').classList.toggle('hide');
+        });
+    });
+
+    const minusIcons = document.querySelectorAll('.minus_icon');
+    Array.from(minusIcons).forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.target.classList.toggle('hide');  
+            e.target.parentElement.nextElementSibling.classList.toggle('hide');
+            e.target.parentElement.querySelector('.plus_icon').classList.remove('hide');
+        });
+    });
+};
+
+fulfillWithText()
+
